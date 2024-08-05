b1c496123d3bd213698694c61f921573db1c72c5
jjwithnosauce <KimisanaKentai@protonmail.com>
2024-08-05 16:52:50 +0700

    Lab7 Submission Supacharn Kaowanan 653380026-4 Section1

---
+++ TestCountClump.py
@@ -0,0 +1,23 @@
+import unittest
+from source import CountClump as CountClump
+
+class TestByC_DC_Coverage(unittest.TestCase):
+    def test_TC01(self):
+        result = CountClump.CountClump.count_clumps([0])
+        self.assertEqual(result, 0)
+
+    def test_TC02(self):
+        result = CountClump.CountClump.count_clumps([1,2,3])
+        self.assertEqual(result, 0)
+
+    def test_TC03(self):
+        result = CountClump.CountClump.count_clumps([1,1,2,3])
+        self.assertEqual(result, 1)
+
+    def test_TC04(self):
+        result = CountClump.CountClump.count_clumps(None)
+        self.assertEqual(result, 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
